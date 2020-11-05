043e933cae6f78c3e6eda3e5d8b72b5116e9a039
Belu Taranto <belentaranto@gmail.com>
2020-11-05 00:48:00 -0300

    Ejercicio 2

---
+++ guia_1/ej02.py
@@ -0,0 +1,7 @@
+"""
+Ejercicio 2: Crear un programa en el cual se almacene en una variable el string "Hola Mundo!" y luego se imprima por pantalla dicha variable.
+"""
+
+msg = "Hola Mundo!"
+
+print(msg)

3bb859399802b614097c4c4dc93a75537b6debae
Belu Taranto <belentaranto@gmail.com>
2020-11-05 01:15:41 -0300

    Ejercicio 8

---
+++ guia_1/ej08.py
@@ -0,0 +1,7 @@
+"""
+Ejercicio 8: Crear un programa que almacene en una lista las siguientes materias: Historia, Matemática, Lengua y Geografía. Luego devolver por pantalla la última materia almacenada.
+"""
+
+asignatures = ["Historia", "Matemática", "Lengua", "Geografía"]
+
+print(asignatures[-1])

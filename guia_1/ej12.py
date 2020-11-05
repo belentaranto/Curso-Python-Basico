8ad0e15843dfd7ce15b9cd5515baf4aedf34362a
Belu Taranto <belentaranto@gmail.com>
2020-11-05 01:52:49 -0300

    Ejercicio 12

---
+++ guia_1/ej12.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 12: Crear un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y muestre por pantalla su producto escalar.
+"""
+
+v1 = [1, 2, 3]
+v2 = [-1, 0, 2]
+
+prod_1 = v1[0] * v2[0]
+prod_2 = v1[1] * v2[1]
+prod_3 = v1[2] * v2[2]
+
+print(f"El producto escalar es de {prod_1 + prod_2 + prod_3}")

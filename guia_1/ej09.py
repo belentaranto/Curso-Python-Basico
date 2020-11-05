2335653293a44741c775fd6c9b5ec32bd4566c32
Belu Taranto <belentaranto@gmail.com>
2020-11-05 01:22:45 -0300

    Ejercicio 9

---
+++ guia_1/ej09.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 9: Crear un programa que pregunte al usuario 5 números enteros y devuelva una lista con ellos.
+"""
+
+n1 = int(input("Por favor ingresá 5 números: "))
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+n5 = int(input())
+
+num_list = [n1, n2, n3, n4, n5]
+
+print(num_list)

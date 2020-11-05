2f89dd058fcae835e156c2594c0320a49794aa6a
Belu Taranto <belentaranto@gmail.com>
2020-11-05 00:51:42 -0300

    Ejercicio 3

---
+++ guia_1/ej03.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 3: Crear un programa que pregunte al usuario su nombre y edad y luego imprima esos datos en renglones distintos.
+"""
+
+name = input("Por favor ingrese su nombre: ")
+age = input("Por favor ingrese su edad: ")
+
+print(name)
+print(age)

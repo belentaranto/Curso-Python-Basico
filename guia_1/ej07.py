911951d5a9e4e710bfdc8453fe901b8ed297727c
Belu Taranto <belentaranto@gmail.com>
2020-11-05 01:11:30 -0300

    Ejercicio 7

---
+++ guia_1/ej07.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 7: Crear un programa que calcule el sueldo bruto de una persona que trabaja de lunes a viernes 8 hs y su pago por hora es de 400 pesos. Devolver el sueldo por pantalla.
+"""
+
+hs_trabajo = 8
+dias_trabajo = 5
+plata_hs = 400
+
+print(f"El sueldo bruto será de ${hs_trabajo * dias_trabajo * plata_hs}")

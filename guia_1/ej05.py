63f468c9382727a111faf75e804d8a754d3e90c4
Belu Taranto <belentaranto@gmail.com>
2020-11-05 01:00:39 -0300

    Ejercicio 5

---
+++ guia_1/ej05.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 5: Crear un programa que pida al usuario ingresar 2 números por teclado y devuelva por pantalla la suma de ellos en un renglón, la resta en otro, el producto en otros y la división en otro.
+"""
+
+num1 = int(input("Por favor ingresa un número: "))
+num2 = int(input("Por favor ingresa otro número: "))
+
+print(f"Suma: {num1 + num2}")
+print(f"Resta: {num1 - num2}")
+print(f"Producto: {num1 * num2}")
+print(f"División: {num1 / num2}")

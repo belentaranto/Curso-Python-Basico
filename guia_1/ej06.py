a965ac271ee1ab59bf5ab1e9a6564669fbc92181
Belu Taranto <belentaranto@gmail.com>
2020-11-05 01:08:37 -0300

    Ejercicio 6

---
+++ guia_1/ej06.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 6: Crear un programa que calcule cuánto dinero tendré al cabo de un mes si deposito hoy 2000 en el banco y el interés mensual es de 5%, y luego devuelva por pantalla ese valor.
+"""
+
+dep = 2000
+suma_interes = dep * 1.05
+
+print(f"Al cabo de un mes tendrás en el banco ${suma_interes}")
